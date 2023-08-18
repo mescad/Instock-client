2ba35fb84a888824c5bb1dca5513a1526116aade
Riccardo <r.golin@hotmail.it>
2023-08-18 14:14:56 +0100

    adding validation

--- src/page/ItemPages/ItemAddPage.js
+++ src/page/ItemPages/ItemAddPage.js
@@ -1,7 +1,51 @@
 import './ItemAddPage.scss';
 import arrowBack from '../../asset/Icons/arrow_back-24px.svg';
+import axios from 'axios';
+const PORT = process.env.REACT_APP_PORT;
+const DOMAIN = process.env.REACT_APP_API_DOMAIN;
 
 function AddInventoryItem() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+
+    if (
+      !form.itemName.value ||
+      !form.category.value ||
+      !form.description.value ||
+      !form.warehouseId.value
+    ) {
+      return;
+    }
+
+    if (form.status.value === 'In Stock' && !form.quantity.value) {
+      return;
+    }
+
+    const newItem = {
+      item_name: form.itemName.value,
+      category: form.category.value,
+      description: form.description.value,
+      status: form.status.value,
+      quantity: parseInt(form.quantity.value),
+      warehouse_id: parseInt(form.warehouseId.value)
+    };
+
+    console.log(newItem);
+
+    axios
+      .post(`${DOMAIN}:${PORT}/api/inventories`, newItem)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  function resetFormInput(e) {
+    e.preventDefault();
+  }
   return (
     <section className="section">
       <div className="section-title-wrapper">
@@ -9,7 +53,12 @@ function AddInventoryItem() {
         <h1 className="section-title-wrapper__title">Add New Inventory Item</h1>
       </div>
       <div className="form-wrapper">
-        <form className="form">
+        <form
+          className="form"
+          onSubmit={e => {
+            handleSubmit(e);
+          }}
+        >
           <div className="another-wrapper">
             <div className="wrapper wrapper--border">
               <h2 className="form__title">Item Details</h2>
@@ -32,12 +81,12 @@ function AddInventoryItem() {
               <label>
                 Category
                 <select name="category" className="input input--select">
-                  <option>Please select</option>
-                  <option>Accessories</option>
-                  <option>Apparel</option>
-                  <option>Electronics</option>
-                  <option>Gear</option>
-                  <option>Health</option>
+                  <option value={null}>Please select</option>
+                  <option value={'Accessories'}>Accessories</option>
+                  <option value={'Apparel'}>Apparel</option>
+                  <option value={'Electronics'}>Electronics</option>
+                  <option value={'Gear'}>Gear</option>
+                  <option value={'Health'}>Health</option>
                 </select>
               </label>
             </div>
@@ -47,11 +96,11 @@ function AddInventoryItem() {
               <h3 className="wrapper__second-title">Status</h3>
               <div className="wrapper-to-flex">
                 <label>
-                  <input type="radio" />
+                  <input type="radio" name="status" value={'In Stock'} />
                   In stock
                 </label>
                 <label>
-                  <input type="radio" />
+                  <input type="radio" name="status" value={'Out of Stock'} />
                   Out of stock
                 </label>
               </div>
@@ -60,6 +109,7 @@ function AddInventoryItem() {
                 <label>
                   Quantity
                   <input
+                    name="quantity"
                     type="number"
                     className="input input--quantity"
                     placeholder="0"
@@ -68,23 +118,32 @@ function AddInventoryItem() {
               </div>
               <label>
                 Warehouse
-                <select className="input input--select">
-                  <option>Please select</option>
-                  <option>Manhattan</option>
-                  <option>Washington</option>
-                  <option>Jersey</option>
-                  <option>SF</option>
-                  <option>Santa Monica</option>
-                  <option>Seattle</option>
-                  <option>Miami</option>
-                  <option>Boston</option>
+                <select name="warehouseId" className="input input--select">
+                  <option value={null}>Please select</option>
+                  <option value={1}>Manhattan</option>
+                  <option value={2}>Washington</option>
+                  <option value={3}>Jersey</option>
+                  <option value={4}>SF</option>
+                  <option value={5}>Santa Monica</option>
+                  <option value={6}>Seattle</option>
+                  <option value={7}>Miami</option>
+                  <option value={8}>Boston</option>
                 </select>
               </label>
             </div>
           </div>
           <div className="button-wrapper">
-            <button className="button">Cancel</button>
-            <button className="button button--blue">+ Add Item</button>
+            <button
+              onClick={e => {
+                resetFormInput(e);
+              }}
+              className="button"
+            >
+              Cancel
+            </button>
+            <button type="submit" className="button button--blue">
+              + Add Item
+            </button>
           </div>
         </form>
       </div>
