03347ebdcd0583ec6b1e3462a0c7f5669fe8db50
Dumitru Mesca <dumitrumesca@gmail.com>
2023-08-18 12:42:11 +0100

    added the delete function for warehouse

--- src/page/WareHousePages/WHomePage.js
+++ src/page/WareHousePages/WHomePage.js
@@ -8,72 +8,83 @@ import ButtonAdd from "../../components/ButtonAdd/ButtonAdd";
 import "./WHomePage.scss";
 
 function WHomePage() {
-	const [warehouses, setWarehouses] = useState(null);
-	const [isLoading, setIsLoading] = useState(true);
-	const tableSetting = [
-		{
-			name: "WAREHOUSE",
-			width: "20%",
-		},
-		{
-			name: "ADDRESS",
-			width: "20%",
-		},
-		{
-			name: "CONTACT NAME",
-			width: "27%",
-		},
-		{
-			name: "CONTACT INFORMATION",
-			width: "33%",
-		},
-	];
-	
-	useEffect(() => {
-		axios
-			.get(`${process.env.REACT_APP_API_DOMAIN}:${process.env.REACT_APP_PORT}/api/warehouses`)
-			.then((res) => {
-				setIsLoading(false);
-				setWarehouses(res.data);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
+  const [warehouses, setWarehouses] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const tableSetting = [
+    {
+      name: "WAREHOUSE",
+      width: "20%",
+    },
+    {
+      name: "ADDRESS",
+      width: "20%",
+    },
+    {
+      name: "CONTACT NAME",
+      width: "27%",
+    },
+    {
+      name: "CONTACT INFORMATION",
+      width: "33%",
+    },
+  ];
 
-	if (isLoading) {
-		return;
-	}
+  const [displayDeleteModal, setDisplayDeleteModal] = useState([]); // set state for delete modal
+  const refreshTableFunc = () => {
+    // refactor useeffect api as a function so that can be pass down to refresh data
+    axios
+      .get(
+        `${process.env.REACT_APP_API_DOMAIN}:${process.env.REACT_APP_PORT}/api/warehouses`
+      )
+      .then((res) => {
+        setIsLoading(false);
+        setWarehouses(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-	return (
-		<>
-			<section className="heading">
-				<h1 className="heading__title">Warehouses</h1>
-				<div className="heading__right-wrapper">
-					<SearchBar />
-					<Link to="/warehouses/add">
-						<ButtonAdd buttonText={"+ Add New Warehouse"} />
-					</Link>
-				</div>
-			</section>
+  useEffect(() => {
+    // refactor useeffect to use refresh table function
+    refreshTableFunc();
+  }, []);
 
-			
-			<section className="warehouse-list">
-				<section className="warehouse-list__table-header">
-					<TableHeader tableSetting={tableSetting} />
-				</section>
-				<div className="warehouse__wrapper">
-					{warehouses.map((warehouse) => (
-						<WarehouseListItem
-							warehouse={warehouse}
-							tableSetting={tableSetting}
-							key={warehouse.id}
-						/>
-					))}
-				</div>
-			</section>
-		</>
-	);
+  if (isLoading) {
+    return;
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1 className="heading__title">Warehouses</h1>
+        <div className="heading__right-wrapper">
+          <SearchBar />
+          <Link to="/warehouses/add">
+            <ButtonAdd buttonText={"+ Add New Warehouse"} />
+          </Link>
+        </div>
+      </section>
+
+      <section className="warehouse-list">
+        <section className="warehouse-list__table-header">
+          <TableHeader tableSetting={tableSetting} />
+        </section>
+        <div className="warehouse__wrapper">
+          {warehouses.map((warehouse) => (
+            <WarehouseListItem // add new props to list items
+              warehouse={warehouse}
+              tableSetting={tableSetting}
+              key={warehouse.id}
+              refreshTableFunc={refreshTableFunc}
+              setDisplayDeleteModal={setDisplayDeleteModal}
+            />
+          ))}
+        </div>
+      </section>
+      {displayDeleteModal.map((deleteModal) => deleteModal)}
+    </>
+  );
 }
 
 export default WHomePage;
